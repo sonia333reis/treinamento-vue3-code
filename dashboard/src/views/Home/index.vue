4f61f7d820e203d652d985ef52d9987bc4838ed9
Sonia Reis <sonia333reis@gmail.com>
2021-02-08 07:50:49 -0300

    feat: criação e estilização do custom header

--- dashboard/src/views/Home/index.vue
+++ dashboard/src/views/Home/index.vue
@@ -1,7 +1,5 @@
 <template>
-  <custom-header
-    @create-account="handleAccountCreate"
-    @login="handleLogin" />
+  <custom-header @create-account="handleAccountCreate" @login="handleLogin" />
   <contact />
   <div class="flex justify-center py-10 bg-brand-gray">
     <p class="font-medium text-center text-gray-800">feedbacker © 2021</p>
